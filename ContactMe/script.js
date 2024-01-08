d8059c18668b0d23f5c7d2faba65e09d6ec1c790
ardamerttarkan <arda.tarkan60@hotmail.com>
2024-01-08 22:41:51 +0300

    completed e mail contact

--- ContactMe/script.js
+++ ContactMe/script.js
@@ -11,9 +11,7 @@ function sendEmail(){
 
 
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "ardamert607734@gmail.com",
-        Password : "3BAC479B961373B99A66652CF706051D5FBE",
+        SecureToken: "42efbe46-83f4-45c5-a93d-5a423015def6",
         To : 'ardamert607734@gmail.com',
         From : "ardamert607734@gmail.com",
         Subject : subject.value,
@@ -30,7 +28,88 @@ function sendEmail(){
       }
     );
 }
+
+function checkInputs() {
+
+const items = document.querySelectorAll(".item");
+
+for(const item of items ){
+    if(item.value == ""){
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+            
+          }
+
+          if(items[1].value !=""){
+            checkEmail();
+          }
+
+          items[1].addEventListener("keyup", () =>{
+            checkEmail();
+          });
+
+
+
+          item.addEventListener("keyup", () =>{
+            if(item.value != ""){
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else{
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+          });
+        
+    }
+}
+
+
+function checkEmail(){
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+
+
+
+    if(!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if(email.value != ""){
+            errorTxtEmail.innerText = "Lütfen geçerli bir email adresi giriniz.";
+        }
+        else{
+            errorTxtEmail.innerText = "Email Boş Olamaz";
+        }
+
+
+
+    }
+    else{
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+
+
+}
+
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    checkInputs();
+
+
+if(!name.classList.contains("error") && !email.classList.contains("error") && !message.classList.contains("error") && !subject.classList.contains("error")){
+
     sendEmail();
+   
+    form.reset();
+    return false;
+}
+
+
+
+
 });
