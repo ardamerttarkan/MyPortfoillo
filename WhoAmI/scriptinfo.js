9a0d1186687bb27c78da4d5a2f9342e9c7a14e1d
ardamerttarkan <arda.tarkan60@hotmail.com>
2024-01-14 22:08:58 +0300

    responsives added

--- WhoAmI/scriptinfo.js
+++ WhoAmI/scriptinfo.js
@@ -1,19 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    grabCursor: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
   function ContactMe(){
     window.location.href ="../ContactMe/contactme.html" ;
